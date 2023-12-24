77e5bf10dee68653ec71d7dd3197cde2cba4102e
ashu-py <88982064+ashu-py@users.noreply.github.com>
2023-12-24 23:29:01 +0530

    Create install.sh
    this script will install the required packages .this script is meant to run in linux not in window 
    
    
    for windows you have to manually install the module/library by your self 
    required libraries are gtts and pypdf2

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Installing required packages "
+pip3 install gTTS pypdf2 
+clear
+echo 'changing permission to excutable'
+chmod +x speak.py
+echo 'the program is ready to be run 
+remember not to put the extentions of file their extention are predefine
+
+command to run the program is  ./speak.py '
